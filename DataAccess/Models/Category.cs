1bb0b16f888dc2fbc1bfacdf518b971c8abc4a60
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2022-12-20 19:10:32 +0500

    Category new property added

--- DataAccess/Models/Category.cs
+++ DataAccess/Models/Category.cs
@@ -7,4 +7,5 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; }
     public int BookId { get; set; }
+    public Category ParentCategory { get; set; }    
 }
