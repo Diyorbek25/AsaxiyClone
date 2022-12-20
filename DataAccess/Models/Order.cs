bc91c3d76943e61d49a341ee946535f9eeabcf27
Diyorbek <diyorbek2506@gmail.com>
2022-12-20 18:24:23 +0500

    Order model added

--- DataAccess/Models/Order.cs
+++ DataAccess/Models/Order.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace DataAccess.Models;
 
-namespace DataAccess.Models
+public class Order
 {
-    internal class Order
-    {
-    }
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int UserId { get; set; }
+    public User User { get; }
 }
