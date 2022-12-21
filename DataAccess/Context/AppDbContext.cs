566cd82464727ef96ed3ee3c67242ddbe068673f
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2022-12-21 08:54:38 +0500

    update

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context;
+
+public class AppDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Server=(localdb)\\mssqllocaldb;Database=AsaxiyDB;Trusted_Connection=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
