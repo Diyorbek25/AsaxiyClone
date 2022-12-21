bc138e017002f52535813467c09ce67167875737
eJwtzLFuwjAUheF38ZzBpTEk3pAISK3YWlbLvrkOTkxsXSeFCPHutVC2oyN9/5NpAEzpJww4MsmwKgWWnIM1G1ObrS2x4ry29QevrBafOy3AoAVWsEjhz7VIK32uJTWtqe4aFGzT7wX1Tojef6XmSOezE0PHm8P+u7+Up6aHJafeRE1LxOwMakLKb4LwPnR7c6OMs/EO1IBLQahbGajLI4ZiTkgSb9r54h5osD7cM8ZHdIRJ6YnJcfb+9foHstpQgg== <axadovravshan@mail.ru>
2022-12-21 08:56:01 +0500

    add configuration

---
+++ DataAccess/Configurations/UserHistoryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+
+public class UserHistoryConfiguration : IEntityTypeConfiguration<UserHistory>
+{
+    public void Configure(EntityTypeBuilder<UserHistory> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
