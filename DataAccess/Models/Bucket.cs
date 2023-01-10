c87644e2545713e4e47beead24767269d3c8fd81
Diyorbek Abdumannobov <113180553+Diyorbek25@users.noreply.github.com>
2023-01-10 16:00:16 +0500

    Delete DataAccess directory

--- DataAccess/Models/Bucket.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccess.Models;
-
-public class Bucket
-{
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public int BookId { get; set; }
-    public int Pieces { get; set; }
-}
--- DataAccess/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccess.Models;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<Book> Books { get; set; }
-    public Category ParentCategory { get; set; }    
-}
