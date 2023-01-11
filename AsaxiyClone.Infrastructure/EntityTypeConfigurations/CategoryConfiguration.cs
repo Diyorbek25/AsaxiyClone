e6230416448349474d549a17e2d15f42472bd2d2
Diyorbek <diyorbek2506@gmail.com>
2023-01-11 15:48:40 +0500

    Category Configuration added

---
+++ AsaxiyClone.Infrastructure/EntityTypeConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AsaxiyClone.Domain.Constants;
+using AsaxiyClone.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AsaxiyClone.Infrastructure.EntityTypeConfigurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable(TableName.Categories);
+
+        builder.HasKey(category => category.Id);
+
+        builder.Property(category => category.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
