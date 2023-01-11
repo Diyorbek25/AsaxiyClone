16a2976013bf36e8f8456379afc2963f2c8c17e7
Diyorbek <diyorbek2506@gmail.com>
2023-01-11 10:08:34 +0500

    AddressConfiguration added

---
+++ AsaxiyClone.Infrastructure/EntityTypeConfigurations/AddressConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AsaxiyClone.Domain.Constants;
+using AsaxiyClone.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AsaxiyClone.Infrastructure.EntityTypeConfigurations;
+
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable(TableName.Addresses);
+
+        builder.HasKey(address => address.Id);
+
+        builder.Property(address => address.Description)
+            .HasMaxLength(150)
+            .IsRequired(false);
+        builder.Property(address => address.PostalCode)
+            .IsRequired(false);
+    }
+}
