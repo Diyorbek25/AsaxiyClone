46fffc601725d7be565f3f48489d84824785f6b6
Diyorbek <diyorbek2506@gmail.com>
2023-01-11 15:26:32 +0500

    User History Configuration added

---
+++ AsaxiyClone.Infrastructure/EntityTypeConfigurations/UserHistoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿
+using AsaxiyClone.Domain.Constants;
+using AsaxiyClone.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AsaxiyClone.Infrastructure.EntityTypeConfigurations;
+
+public class UserHistoryConfiguration : IEntityTypeConfiguration<UserHistory>
+{
+    public void Configure(EntityTypeBuilder<UserHistory> builder)
+    {
+        builder.ToTable(TableName.Histories);
+
+        builder.HasKey(history => history.Id);
+
+        builder.HasOne<User>(history => history.User)
+            .WithMany(user => user.UserHistories)
+            .HasForeignKey(history => history.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Order>(history => history.Order)
+            .WithMany(order => order.UserHistories)
+            .HasForeignKey(history => history.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
