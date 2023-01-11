4dce329573b56d278c5e84ad8a280a1bede1de3d
Diyorbek <diyorbek2506@gmail.com>
2023-01-11 15:48:01 +0500

    Basket Configuration added

--- AsaxiyClone.Infrastructure/EntityTypeConfigurations/BasketConfiguration.cs
+++ AsaxiyClone.Infrastructure/EntityTypeConfigurations/BasketConfiguration.cs
@@ -1,9 +1,30 @@
 ﻿
+using AsaxiyClone.Domain.Constants;
 using AsaxiyClone.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AsaxiyClone.Infrastructure.EntityTypeConfigurations;
 
-public class BasketConfiguration
+public class BasketConfiguration : IEntityTypeConfiguration<Basket>
 {
+    public void Configure(EntityTypeBuilder<Basket> builder)
+    {
+        builder.ToTable(TableName.Baskets);
 
+        builder.HasKey(basket => basket.Id);
+
+        builder.Property(basket => basket.Pieces)
+            .IsRequired();
+
+        builder.HasOne<User>(basket => basket.User)
+            .WithOne(user => user.Basket)
+            .HasForeignKey<Basket>(basket => basket.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Book>(basket => basket.Book)
+            .WithOne()
+            .HasForeignKey<Basket>(basket => basket.BookId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
